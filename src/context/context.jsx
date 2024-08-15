26465f6853159834d7d2696f4410a023012d81b3
Nishant Kumar Gupta <nishantkumargupta728@gmail.com>
2024-08-15 14:39:12 +0530

    add the typing effect and apperance

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -11,13 +11,56 @@ const ContextProvider = (props) => {
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState(" ");
 
+  // for typing effect
+  const delaypara = (index, nextWord) => {
+    setTimeout(function () {
+      setResultData(prev => prev + nextWord);
+    }, 75*index)
+  }
+
+  const newChat = () =>{
+    setLoading(false)
+    setShowResult(false)
+  }
+
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
     setShowResult(true);
-    setRecentPrompt(input);
-    const response = await run(input);
-    setResultData(response);
+    let response;
+    if(prompt !== undefined){
+      response = await run(prompt)
+      setRecentPrompt(prompt);
+    }
+    else{
+      setPrevPrompts(prev =>[...prev,input])
+      setRecentPrompt(input)
+      response = await run(input)
+    }
+
+    // save prev data prompt
+    
+    // const response = await run(input);
+
+    // this is for doing boldness in generated text on highlights points
+    let responseArray = response.split("**");
+    let newResponse ="";
+    for(let i = 0; i< responseArray.length; i++){
+      if(i === 0 || i%2 !== 1){
+        newResponse += responseArray[i];
+      }
+      else{
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+    let newResponse2 = newResponse.split("*").join("</br>");
+    // setResultData(newResponse2);
+    let newResponseArray = newResponse2.split(" ");
+    for(let i=0; i< newResponseArray.length ;i++){
+      const nextWord = newResponseArray[i];
+      delaypara(i,nextWord + " ");
+    }
+
     setLoading(false);
     setInput("");
   }
@@ -34,6 +77,7 @@ const ContextProvider = (props) => {
     resultData,
     input,
     setInput,
+    newChat,
   };
 
   return (
